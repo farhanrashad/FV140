207cb73628d995aa3e947c16b7898ceb6ddc2ba1
ahmadraza47 <38661176+ahmadraza47@users.noreply.github.com>
2021-09-18 18:01:37 +0500

    de_inventory_vehicle_info

---
+++ de_inventory_vehicle_info/models/stock.py
@@ -0,0 +1,8 @@
+from odoo import api, fields, models, _
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    vehicle_number = fields.Char('Vehicle No.')
+    driver = fields.Char('Driver')
