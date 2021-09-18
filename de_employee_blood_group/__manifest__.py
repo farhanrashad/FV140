847c33824ecb083034217a9b86850995dd3ade53
ahmadraza47 <38661176+ahmadraza47@users.noreply.github.com>
2021-09-18 17:27:58 +0500

    de_employee_blood_group

---
+++ de_employee_blood_group/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Employee Blood Group',
+    'version': '13.0.0.1',
+    'author': 'DX',
+    'website': '',
+    'category': '',
+    'description': """Employee Blood Group""",
+    'depends': ['hr'],
+    'data': [
+        'views/employee_view.xml'
+    ],
+    'installable': True,
+}
