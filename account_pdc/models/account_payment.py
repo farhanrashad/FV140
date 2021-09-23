6a64485c006b0ae6b5eb8397405ff63626e5a752
ahmadraza47 <38661176+ahmadraza47@users.noreply.github.com>
2021-09-23 07:44:50 +0500

    update pdc

--- account_pdc/models/account_payment.py
+++ account_pdc/models/account_payment.py
@@ -3,28 +3,28 @@ from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 
 
-# class AccountRegisterPayments(models.TransientModel):
-#     _inherit = "account.register.payments"
-#
-#     bank_reference = fields.Char(copy=False)
-#     cheque_reference = fields.Char(copy=False)
-#     effective_date = fields.Date('Effective Date', help='Effective date of PDC', copy=False, default=False)
-#
-#     def get_payment_vals(self):
-#         res = super(AccountRegisterPayments, self).get_payment_vals()
-#         if self.payment_method_id == self.env.ref('account_check_printing.account_payment_method_check'):
-#             res.update({
-#                 'check_amount_in_words': self.check_amount_in_words,
-#                 'check_manual_sequencing': self.check_manual_sequencing,
-#                 'effective_date': self.effective_date,
-#             })
-#         return res
-#
-#
-# class AccountPaymentInherit(models.Model):
-#     _inherit = "account.payment"
-#
-#     effective_date = fields.Date('Effective Date', help='Effective date of PDC', copy=False, default=False)
+class AccountRegisterPayments(models.TransientModel):
+    _inherit = "account.register.payments"
+
+    bank_reference = fields.Char(copy=False)
+    cheque_reference = fields.Char(copy=False)
+    effective_date = fields.Date('Effective Date', help='Effective date of PDC', copy=False, default=False)
+
+    def get_payment_vals(self):
+        res = super(AccountRegisterPayments, self).get_payment_vals()
+        if self.payment_method_id == self.env.ref('account_check_printing.account_payment_method_check'):
+            res.update({
+                'check_amount_in_words': self.check_amount_in_words,
+                'check_manual_sequencing': self.check_manual_sequencing,
+                'effective_date': self.effective_date,
+            })
+        return res
+
+
+class AccountPaymentInherit(models.Model):
+    _inherit = "account.payment"
+
+    effective_date = fields.Date('Effective Date', help='Effective date of PDC', copy=False, default=False)
 
 
 class AccountPayment(models.Model):
