6d434071ef8658948b81ddedf21ae6aa5a1b17cc
ahmadraza47 <38661176+ahmadraza47@users.noreply.github.com>
2021-10-05 23:37:59 +0500

    update sale claim

--- sale_product_exchange_drc/models/account_invoice.py
+++ sale_product_exchange_drc/models/account_invoice.py
@@ -6,13 +6,13 @@ class AccountInvoice(models.Model):
 
     claim_id = fields.Many2one('sale.claim')
 
-    # @api.multi
-    def open_claim_invoice(self):
-        action = {}
-        action_id = self.env.ref('account.action_invoice_tree2')
-        if action_id:
-            action = action_id.read()
-            action = action[0] or action
-            action['domain'] = "[('id','in', [" + ','.join(map(str, self.ids)) + "])]"
-            return action
-        return True
+    # # @api.multi
+    # def open_claim_invoice(self):
+    #     action = {}
+    #     action_id = self.env.ref('account.action_invoice_tree2')
+    #     if action_id:
+    #         action = action_id.read()
+    #         action = action[0] or action
+    #         action['domain'] = "[('id','in', [" + ','.join(map(str, self.ids)) + "])]"
+    #         return action
+    #     return True
