bafbabb6a7ebe25b03b86b3e36fe02066e9e43f6
ahmadraza47 <38661176+ahmadraza47@users.noreply.github.com>
2021-10-06 05:12:37 +0500

    update sh-pos order list

--- sh_pos_order_list/models/pos_config.py
+++ sh_pos_order_list/models/pos_config.py
@@ -10,10 +10,8 @@ _logger = logging.getLogger(__name__)
 class PosOrder(models.Model):
     _inherit = 'pos.order'
 
-    assigned_config = fields.Many2many(
-        "pos.config", string=" Sh Assigned Config")
-    sequence_number = fields.Integer(
-        string='Sequence Number', help='A session-unique sequence number for the order', default=1)
+    assigned_config = fields.Many2many("pos.config", string=" Sh Assigned Config")
+    sequence_number = fields.Integer(string='Sequence Number', help='A session-unique sequence number for the order', default=1)
     sh_uid = fields.Char(string='Number')
     sh_order_line_id = fields.Char(string='Number')
     sh_order_date = fields.Char(string="Order Date")
@@ -21,13 +19,9 @@ class PosOrder(models.Model):
     @api.model
     def search_order_length(self, config_data):
         if config_data['sh_load_order_by']:
-
             if config_data['sh_load_order_by'] == 'session_wise':
-
                 if config_data['sh_session_wise_option'] == 'current_session':
-                    order_data = self.env['pos.order'].search_read(['|', ('user_id', '=', self.env.user.id), (
-                        'assigned_config', '=', config_data['id']), ('session_id', '=', config_data['current_session_id'][0]), ('state', '!=', 'cancel')])
-
+                    order_data = self.env['pos.order'].search_read(['|', ('user_id', '=', self.env.user.id), ('assigned_config', '=', config_data['id']), ('session_id', '=', config_data['current_session_id'][0]), ('state', '!=', 'cancel')])
                 if config_data['sh_session_wise_option'] == 'last_no_session':
                     all_session = self.env['pos.session'].search_read([])
                     for index, obj in enumerate(all_session):
@@ -42,28 +36,21 @@ class PosOrder(models.Model):
                         if x < len(all_session):
                             session.append(all_session[x]['id'])
                     if session:
-                        order_data = self.env['pos.order'].search_read(['|', ('user_id', '=', self.env.user.id), (
-                            'assigned_config', '=', config_data['id']), ('session_id', 'in', session), ('state', '!=', 'cancel')])
+                        order_data = self.env['pos.order'].search_read(['|', ('user_id', '=', self.env.user.id), ('assigned_config', '=', config_data['id']), ('session_id', 'in', session), ('state', '!=', 'cancel')])
 
             if config_data['sh_load_order_by'] == 'all':
-                order_data = self.env['pos.order'].search_read(['|', ('user_id', '=', self.env.user.id), (
-                    'assigned_config', '=', config_data['id']), ('state', '!=', 'cancel')])
+                order_data = self.env['pos.order'].search_read(['|', ('user_id', '=', self.env.user.id), ('assigned_config', '=', config_data['id']), ('state', '!=', 'cancel')])
 
             if config_data['sh_load_order_by'] == 'day_wise':
-
                 if config_data['sh_day_wise_option'] == 'current_day':
                     today_date = datetime.today().strftime('%Y-%m-%d')
-                    order_data = self.env['pos.order'].search_read(['|', ('user_id', '=', self.env.user.id), ('assigned_config', '=', config_data['id']), (
-                        'date_order', '>=', (today_date + " 00:00:00")), ('date_order', '<=', (today_date + " 24:00:00")), ('state', '!=', 'cancel')])
-
+                    order_data = self.env['pos.order'].search_read(['|', ('user_id', '=', self.env.user.id), ('assigned_config', '=', config_data['id']), ('date_order', '>=', (today_date + " 00:00:00")), ('date_order', '<=', (today_date + " 24:00:00")), ('state', '!=', 'cancel')])
                 if config_data['sh_day_wise_option'] == 'last_no_day':
                     if config_data['sh_last_no_days']:
                         today_date = datetime.today().strftime('%Y-%m-%d')
-                        last_date = datetime.today() - \
-                            timedelta(days=config_data['sh_last_no_days'])
+                        last_date = datetime.today() - \ timedelta(days=config_data['sh_last_no_days'])
                         last_date = last_date.strftime('%Y-%m-%d')
-                        order_data = self.env['pos.order'].search_read(['|', ('user_id', '=', self.env.user.id), ('assigned_config', '=', config_data['id']), (
-                            'date_order', '<=', (today_date + " 24:00:00")), ('date_order', '>', (last_date + " 24:00:00")), ('state', '!=', 'cancel')])
+                        order_data = self.env['pos.order'].search_read(['|', ('user_id', '=', self.env.user.id), ('assigned_config', '=', config_data['id']), ('date_order', '<=', (today_date + " 24:00:00")), ('date_order', '>', (last_date + " 24:00:00")), ('state', '!=', 'cancel')])
         order_line = []
         if order_data and len(order_data) > 0:
             order_ids = []
@@ -75,15 +62,13 @@ class PosOrder(models.Model):
                         if payment_obj and payment_obj[0]:
                             each_order['payment_data'].append(payment_obj[0])
                 order_ids.append(each_order.get('id'))
-            order_line = self.env['pos.order.line'].search_read(
-                [('order_id', 'in', order_ids)])
+            order_line = self.env['pos.order.line'].search_read([('order_id', 'in', order_ids)])
 
         return {'order': order_data, 'order_line': order_line}
 
     @api.model
     def search_order(self, config_data, page_number):
-        showFrom = int(
-            config_data['sh_how_many_order_per_page']) * (int(page_number) - 1)
+        showFrom = int(config_data['sh_how_many_order_per_page']) * (int(page_number) - 1)
         showTo = showFrom + int(config_data['sh_how_many_order_per_page'])
 
         if config_data['sh_load_order_by']:
@@ -91,8 +76,7 @@ class PosOrder(models.Model):
             if config_data['sh_load_order_by'] == 'session_wise':
 
                 if config_data['sh_session_wise_option'] == 'current_session':
-                    order_data = self.env['pos.order'].search_read(['|', ('user_id', '=', self.env.user.id), ('assigned_config', '=', config_data['id']), (
-                        'session_id', '=', config_data['current_session_id'][0]), ('state', '!=', 'cancel')], limit=showTo)
+                    order_data = self.env['pos.order'].search_read(['|', ('user_id', '=', self.env.user.id), ('assigned_config', '=', config_data['id']), ('session_id', '=', config_data['current_session_id'][0]), ('state', '!=', 'cancel')], limit=showTo)
 
                 if config_data['sh_session_wise_option'] == 'last_no_session':
                     all_session = self.env['pos.session'].search_read([])
@@ -108,28 +92,23 @@ class PosOrder(models.Model):
                         if x < len(all_session):
                             session.append(all_session[x]['id'])
                     if session:
-                        order_data = self.env['pos.order'].search_read(['|', ('user_id', '=', self.env.user.id), (
-                            'assigned_config', '=', config_data['id']), ('session_id', 'in', session), ('state', '!=', 'cancel')], limit=showTo)
+                        order_data = self.env['pos.order'].search_read(['|', ('user_id', '=', self.env.user.id), ('assigned_config', '=', config_data['id']), ('session_id', 'in', session), ('state', '!=', 'cancel')], limit=showTo)
 
             if config_data['sh_load_order_by'] == 'all':
-                order_data = self.env['pos.order'].search_read(['|', ('user_id', '=', self.env.user.id), (
-                    'assigned_config', '=', config_data['id']), ('state', '!=', 'cancel')], limit=showTo)
+                order_data = self.env['pos.order'].search_read(['|', ('user_id', '=', self.env.user.id), ('assigned_config', '=', config_data['id']), ('state', '!=', 'cancel')], limit=showTo)
 
             if config_data['sh_load_order_by'] == 'day_wise':
 
                 if config_data['sh_day_wise_option'] == 'current_day':
                     today_date = datetime.today().strftime('%Y-%m-%d')
-                    order_data = self.env['pos.order'].search_read(['|', ('user_id', '=', self.env.user.id), ('assigned_config', '=', config_data['id']), (
-                        'date_order', '>=', (today_date + " 00:00:00")), ('date_order', '<=', (today_date + " 24:00:00")), ('state', '!=', 'cancel')], limit=showTo)
+                    order_data = self.env['pos.order'].search_read(['|', ('user_id', '=', self.env.user.id), ('assigned_config', '=', config_data['id']), ('date_order', '>=', (today_date + " 00:00:00")), ('date_order', '<=', (today_date + " 24:00:00")), ('state', '!=', 'cancel')], limit=showTo)
 
                 if config_data['sh_day_wise_option'] == 'last_no_day':
                     if config_data['sh_last_no_days']:
                         today_date = datetime.today().strftime('%Y-%m-%d')
-                        last_date = datetime.today() - \
-                            timedelta(days=config_data['sh_last_no_days'])
+                        last_date = datetime.today() - \ timedelta(days=config_data['sh_last_no_days'])
                         last_date = last_date.strftime('%Y-%m-%d')
-                        order_data = self.env['pos.order'].search_read(['|', ('user_id', '=', self.env.user.id), ('assigned_config', '=', config_data['id']), (
-                            'date_order', '<=', (today_date + " 24:00:00")), ('date_order', '>', (last_date + " 24:00:00")), ('state', '!=', 'cancel')], limit=showTo)
+                        order_data = self.env['pos.order'].search_read(['|', ('user_id', '=', self.env.user.id), ('assigned_config', '=', config_data['id']), ('date_order', '<=', (today_date + " 24:00:00")), ('date_order', '>', (last_date + " 24:00:00")), ('state', '!=', 'cancel')], limit=showTo)
         order_data = order_data[showFrom:showTo]
         order_line = []
         if order_data and len(order_data) > 0:
@@ -167,36 +146,28 @@ class PosConfig(models.Model):
     sh_enable_order_reprint = fields.Boolean(string="Allow To Reprint Order")
     sh_enable_re_order = fields.Boolean(string="Allow To ReOrder")
     sh_enable_order_list = fields.Boolean(string="Enable Order List")
-    sh_load_order_by = fields.Selection(
-        [('all', 'All'), ('session_wise', 'Session Wise'), ('day_wise', 'Day Wise')], string="Load Order By", default='all')
-    sh_session_wise_option = fields.Selection(
-        [('current_session', 'Current Session'), ('last_no_session', 'Last No Of Session')], string="Session Of")
-    sh_day_wise_option = fields.Selection(
-        [('current_day', 'Current Day'), ('last_no_day', 'Last No Of Days')], string="Day Of")
+    sh_load_order_by = fields.Selection([('all', 'All'), ('session_wise', 'Session Wise'), ('day_wise', 'Day Wise')], string="Load Order By", default='all')
+    sh_session_wise_option = fields.Selection([('current_session', 'Current Session'), ('last_no_session', 'Last No Of Session')], string="Session Of")
+    sh_day_wise_option = fields.Selection([('current_day', 'Current Day'), ('last_no_day', 'Last No Of Days')], string="Day Of")
     sh_last_no_days = fields.Integer(string="Last No Of Days")
     sh_last_no_session = fields.Integer(string="Last No Of Session")
     sh_how_many_order_per_page = fields.Integer(
         string="How Many Orders You Want to display Per Page ? ", default=30)
-    sh_mode = fields.Selection(
-        [('online_mode', 'Online'), ('offline_mode', 'Offline')], string="Update List", default='offline_mode')
+    sh_mode = fields.Selection([('online_mode', 'Online'), ('offline_mode', 'Offline')], string="Update List", default='offline_mode')
 
     @api.constrains('sh_last_no_session', 'sh_last_no_days')
     def _check_validity_constrain(self):
         """ verifies if record.to_hrs is earlier than record.from_hrs. """
         for record in self:
             if self.sh_last_no_days < 0:
-                raise ValidationError(
-                    _('Last Number Of Days must be positive.'))
+                raise ValidationError(_('Last Number Of Days must be positive.'))
             if self.sh_last_no_session < 0:
-                raise ValidationError(
-                    _('Last Number Of Sessions must be positive.'))
+                raise ValidationError(_('Last Number Of Sessions must be positive.'))
 
     @api.constrains('sh_how_many_order_per_page')
     def _onchange_sh_how_many_order_per_page(self):
         if self.sh_how_many_order_per_page:
             if self.sh_how_many_order_per_page < 0:
-                raise ValidationError(_('Order Per Page must be positive'
-                                        ))
+                raise ValidationError(_('Order Per Page must be positive'))
         if self.sh_how_many_order_per_page == 0:
-            raise ValidationError(_('Order Per Page must be more than 0'
-                                    ))
+            raise ValidationError(_('Order Per Page must be more than 0'))
