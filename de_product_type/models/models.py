ef5648650dcacc98a63e0f6b67a6bb37eb5d54d2
ahmadraza47 <38661176+ahmadraza47@users.noreply.github.com>
2021-10-07 03:52:34 +0500

    remove

--- de_product_type/models/models.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-    
-    type = fields.Selection([
-        ('service', 'Service'),
-        ('product', 'Stockable Product'),
-    ], string='Product Type', index=True, copy=False, default='product', required=True, track_visibility='onchange')
