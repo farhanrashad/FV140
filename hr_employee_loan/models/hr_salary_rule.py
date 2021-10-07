94f21c2d9fa866e8001270ec50335c50b4a8acb1
ahmadraza47 <38661176+ahmadraza47@users.noreply.github.com>
2021-10-08 00:19:53 +0500

    employee loan

--- hr_employee_loan/models/hr_salary_rule.py
+++ hr_employee_loan/models/hr_salary_rule.py
@@ -2,9 +2,9 @@
 
 from odoo import models, fields
 
-class HrSalaryRule(models.Model):
-    _inherit = 'hr.salary.rule'
-    
-    is_loan_payment = fields.Boolean(
-        string='Loan Payment'
-    )
+# class HrSalaryRule(models.Model):
+#     _inherit = 'hr.salary.rule'
+#
+#     is_loan_payment = fields.Boolean(
+#         string='Loan Payment'
+#     )
