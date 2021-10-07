a495ae9c5e7380e47d763868cdcfb0039a80bc3c
ahmadraza47 <38661176+ahmadraza47@users.noreply.github.com>
2021-10-07 17:08:40 +0500

    internal requisition

--- material_internal_requisitions/models/hr_employee.py
+++ material_internal_requisitions/models/hr_employee.py
@@ -7,7 +7,7 @@ class HrEmployee(models.Model):
 
     desti_loca_id = fields.Many2one(
         'stock.location',
-        string='Destination Location',
+        string='Location Destination',
     )
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
