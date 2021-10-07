994c97733ebc061fbba8045b46c394d9724069cd
ahmadraza47 <38661176+ahmadraza47@users.noreply.github.com>
2021-10-07 05:46:50 +0500

    vehicle info

---
+++ de_inventory_vehicle_info/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'DE inventory vehicle info',
+    'version': '13.0.0.0',
+    'author': 'Dynexcel',
+    'website': '',
+    'category': '',
+    'description': """This module adds extra fields on Stock Form.""",
+    'depends': ['base', 'stock'],
+    'data': [
+        'views/stock_view.xml'
+    ],
+    'installable': True,
+}
