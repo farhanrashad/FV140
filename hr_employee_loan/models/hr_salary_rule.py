bebe04d75d8d4f7f50847158f91dec4651c41a24
ahmadraza47 <38661176+ahmadraza47@users.noreply.github.com>
2021-10-07 16:13:37 +0500

    inventory revulation and employee_loan

--- hr_employee_loan/models/hr_salary_rule.py
+++ hr_employee_loan/models/hr_salary_rule.py
@@ -4,7 +4,7 @@ from odoo import models, fields
 
 class HrSalaryRule(models.Model):
     _inherit = 'hr.salary.rule'
-
+    
     is_loan_payment = fields.Boolean(
         string='Loan Payment',
     )
