027dde5899d03c1de337b8d41bf283bf746696ba
ahmadraza47 <38661176+ahmadraza47@users.noreply.github.com>
2021-10-07 18:25:42 +0500

    stock valuation

--- stock_inventory_revaluation/models/account_move.py
+++ stock_inventory_revaluation/models/account_move.py
@@ -8,42 +8,40 @@
 # from odoo.exceptions import UserError
 #
 #
-# class AccountMove(models.Model):
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    stock_inventory_revaluation_id = fields.Many2one(
+        comodel_name='stock.inventory.revaluation',
+        string='Stock Inventory Revaluation',
+        ondelete='restrict', copy=False)
+
+    # @api.multi
+    def unlink(self):
+        for rec in self:
+            if rec.stock_inventory_revaluation_id and not \
+                    self.env.context.get('revaluation', False):
+                raise UserError(
+                    _("You cannot remove the journal item that is related "
+                      "to an inventory revaluation"))
+        return super(AccountMove, self).unlink()
 #
-#     _inherit = 'account.move'
 #
-#     stock_inventory_revaluation_id = fields.Many2one(
-#         comodel_name='stock.inventory.revaluation',
-#         string='Stock Inventory Revaluation',
-#         ondelete='restrict', copy=False)
-#
-#     # @api.multi
-#     def unlink(self):
-#         for rec in self:
-#             if rec.stock_inventory_revaluation_id and not \
-#                     self.env.context.get('revaluation', False):
-#                 raise UserError(
-#                     _("You cannot remove the journal item that is related "
-#                       "to an inventory revaluation"))
-#         return super(AccountMove, self).unlink()
-#
-#
-# class AccountMoveLine(models.Model):
-#
-#     _inherit = 'account.move.line'
-#
-#     stock_inventory_revaluation_id = fields.Many2one(
-#         comodel_name='stock.inventory.revaluation',
-#         related='move_id.stock_inventory_revaluation_id',
-#         string='Stock Inventory Revaluation',
-#         store=True, ondelete='restrict', copy=False, readonly=True)
-#
-#     # @api.multi
-#     def unlink(self):
-#         for rec in self:
-#             if rec.stock_inventory_revaluation_id and 'revaluation' not in \
-#                     self.env.context:
-#                 raise UserError(
-#                     _("You cannot remove the journal item that is related "
-#                       "to an inventory revaluation"))
-#         return super(AccountMoveLine, self).unlink()
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    stock_inventory_revaluation_id = fields.Many2one(
+        comodel_name='stock.inventory.revaluation',
+        related='move_id.stock_inventory_revaluation_id',
+        string='Stock Inventory Revaluation',
+        store=True, ondelete='restrict', copy=False, readonly=True)
+
+    # @api.multi
+    def unlink(self):
+        for rec in self:
+            if rec.stock_inventory_revaluation_id and 'revaluation' not in \
+                    self.env.context:
+                raise UserError(
+                    _("You cannot remove the journal item that is related "
+                      "to an inventory revaluation"))
+        return super(AccountMoveLine, self).unlink()
