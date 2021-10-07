cd06775f1dd6d2ef35432c09489df1d54b75dff6
ahmadraza47 <38661176+ahmadraza47@users.noreply.github.com>
2021-10-08 01:21:28 +0500

    loan

--- hr_employee_loan/models/hr_salary_rule.py
+++ hr_employee_loan/models/hr_salary_rule.py
@@ -2,9 +2,8 @@
 
 from odoo import models, fields
 
-# class HrSalaryRule(models.Model):
-#     _inherit = 'hr.salary.rule'
-#
-#     is_loan_payment = fields.Boolean(
-#         string='Loan Payment'
-#     )
+class HrSalaryRule(models.Model):
+    _inherit = 'hr.salary.rule'
+
+    struct_id = fields.Many2one('hr.payroll.structure', string="Salary Structure", required=False, ondelete='cascade')
+    is_loan_payment = fields.Boolean(string='Loan Payment')
