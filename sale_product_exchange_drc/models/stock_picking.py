bf4a46086d2a31e2d685963fbfd8dcaaba776af9
ahmadraza47 <38661176+ahmadraza47@users.noreply.github.com>
2021-10-14 00:31:24 +0500

    remove all modules

--- sale_product_exchange_drc/models/stock_picking.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models
-
-
-class StockPickingExt(models.Model):
-    _inherit = 'stock.picking'
-
-    claim_id = fields.Many2one('sale.claim', string='Claim')
